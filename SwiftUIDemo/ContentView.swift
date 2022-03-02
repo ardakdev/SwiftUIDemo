3a72a78ccbcfe59971783f768a720b98361d2fc7
Ardak Boranov <ardakb@gmail.com>
2022-03-02 21:42:36 +0600

    05. Gestures and Events

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -9,10 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     @State var show = false
+    @State var viewState: CGSize = .zero
 
     var body: some View {
         ZStack {
-
             TitleView()
                 .blur(radius: show ? 20 : 0)
                 .animation(.default, value: show)
@@ -22,6 +22,7 @@ struct ContentView: View {
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y:show ? -400 : -40)
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.9)
                 .rotationEffect(Angle(degrees: show ? 0 : 10))
                 .rotation3DEffect(Angle(degrees: 10), axis: (x: 10.0, y: 0, z: 0))
@@ -33,16 +34,29 @@ struct ContentView: View {
                 .cornerRadius(20)
                 .shadow(radius: 20)
                 .offset(x: 0, y: show ? -200 : -20)
+                .offset(x: viewState.width, y: viewState.height)
                 .scaleEffect(0.95)
                 .rotationEffect(Angle(degrees: show ? 0 : 5))
                 .rotation3DEffect(Angle(degrees: 5), axis: (x: 10.0, y: 0, z: 0))
                 .blendMode(.hardLight)
                 .animation(.easeInOut(duration: 0.3), value: show)
 
-
             CardView()
+                .offset(x: viewState.width, y: viewState.height)
                 .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0), value: viewState)
                 .onTapGesture { self.show.toggle() }
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            self.viewState = value.translation
+                            self.show = true
+                        }
+                        .onEnded{ _ in
+                            self.viewState = .zero
+                            self.show = false
+                        }
+                )
 
             BottomCardView()
                 .blur(radius: show ? 20 : 0)
